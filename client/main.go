643e3da3b86f0bc6fa36192103b1f857159564d1
Taras Glek <taras@glek.net>
2023-08-06 15:04:38 +0300

    works

--- client/main.go
+++ client/main.go
@@ -7,34 +7,30 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/pion/webrtc/v3"
 )
-
-func printAsJSON(kind string, msg string) string {
-	msgJSON, _ := json.Marshal(msg)
-	fmt.Println(kind, string(msgJSON))
-	return msg
-}
 
 type CommandResult struct {
 	Output string `json:"output"`
 	Error  string `json:"error,omitempty"`
 }
 
-func runCommandWithTimeout(cmd string, timeout time.Duration) string {
+func runCommandWithTimeout(shell []string, cmd string, timeout time.Duration) string {
 	resultChan := make(chan CommandResult)
 	errorChan := make(chan error)
 	// Run the command in a goroutine so we can kill it if it times out
 	go func() {
 		var command *exec.Cmd
 
-		if runtime.GOOS == "windows" {
-			command = exec.Command("cmd", "/C", cmd)
-		} else {
-			command = exec.Command("/bin/sh", "-c", cmd)
-		}
+		shell_and_cmd := make([]string, len(shell))
+		// Copy the elements from the slice to the array
+		copy(shell_and_cmd, shell)
+		shell_and_cmd = append(shell_and_cmd, cmd)
+
+		command = exec.Command(shell_and_cmd[0], shell_and_cmd[1:]...)
 
 		output, err := command.CombinedOutput()
 		if err != nil {
@@ -77,6 +73,29 @@ func promptAndRead(prompt string) string {
 }
 
 func main() {
+	// Slice off the first argument (program name)
+
+	// If args is empty, set it to ["/bin/sh", "-c"]
+	var default_shell []string
+	if runtime.GOOS == "windows" {
+		default_shell = []string{"cmd", "/C"}
+	} else {
+		default_shell = []string{"/bin/sh", "-c"}
+	}
+
+	// handle -h
+	if len(os.Args) == 2 && os.Args[1] == "-h" {
+		fmt.Println("Usage: " + os.Args[0] + " [shell]")
+		// println shell array joined as string as example
+		// use strings.Join(shell, " ")
+		fmt.Println("Example: " + os.Args[0] + " " + strings.Join(default_shell, " "))
+		os.Exit(0)
+	}
+	shell := os.Args[1:]
+	if len(shell) == 0 {
+		shell = default_shell
+	}
+
 	// Prepare the configuration
 	config := webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
@@ -109,18 +128,15 @@ func main() {
 		msgStr := string(msg.Data)
 		msgJSON, _ := json.Marshal(msgStr)
 		fmt.Println("OnMessage", string(msgJSON))
-		ret := runCommandWithTimeout(string(msg.Data), 1*time.Second)
+		ret := runCommandWithTimeout(default_shell, string(msg.Data), 1*time.Second)
 		dataChannel.SendText(ret)
 	})
 
 	// Handle ICE connection state changes
 	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
 		fmt.Println("ICE Connection State has changed:", connectionState.String())
-		if connectionState == webrtc.ICEConnectionStateConnected {
-			// send fake bash prompt
-			dataChannel.SendText("bash$ ")
-		}
-
+		// if connectionState == webrtc.ICEConnectionStateConnected {
+		// }
 	})
 
 	offerJSON := promptAndRead("Enter offer from web: ")
